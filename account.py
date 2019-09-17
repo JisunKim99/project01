a60ae09758f0023fccd78b028458728718cd518d
spoon096 <44830767+spoon096@users.noreply.github.com>
2019-09-17 15:56:03 +0900

    This is my first commit

---
+++ account.py
@@ -0,0 +1,12 @@
+def account_info():
+    details = {
+        'Jimmy': {
+        'amount': 1000,
+        'bank_name': 'barclays'
+        },
+        'Jisun': {
+        'amount': 1001,
+        'bank_name': 'Shinhan'
+        }
+    }
+    return details
