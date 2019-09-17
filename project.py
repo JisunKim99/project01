b94f43e1f8e8fafed46986b7909852b022b1cd44
spoon096 <44830767+spoon096@users.noreply.github.com>
2019-09-17 17:20:10 +0900

    Learned hot to use Jinja tool

--- project.py
+++ project.py
@@ -3,30 +3,30 @@ from account import account_info
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
-def hello():
-    if request.method == "POST":
-        return "you called the post method"
-    else:
-        details = account_info()
-        return 'The balance is  and the bank is {details[1]}'
 
-@app.route('/account_info/<name>')
-def get_account(name):
-    details = account_info()
-    return f"The balance for {name} is {details[name]['amount']} and the bank is {details[name]['bank_name']}"
-
-
-@app.route('/user/<username>')
-def show_user_profile(username):
-    # show the user profile for that user
-    return 'User %s' % escape(username)
-
-    from flask import render_template
-
-@app.route('/hello/')
-@app.route('/hello/<name>')
+@app.route('/bank/')
+@app.route('/bank/<name>')
 def home(name=None):
-    return render_template('home.html')
+
+    if name == None:
+        bank_details = account_info()
+        return render_template(
+            'home.html',
+            bank_details=bank_details
+            )
+
+    else:
+        bank_details = account_info()
+        one_person = bank_details[name]
+        amount = one_person['amount']
+        bank_name= one_person['bank_name']
+
+
+        return render_template(
+            'one_person.html',
+            amount = amount,
+            bank_name = bank_name,
+            person_name = name
+            )
 
 
